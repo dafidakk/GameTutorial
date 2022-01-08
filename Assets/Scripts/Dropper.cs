ce5fcb38d6b8ed31c556f1addd68ad4933f03b80
Ahmet Fatih DEVECI <ahmetfatihdeveci@gmail.com>
2022-01-08 14:14:46 +0300

    obsctacle game finish

--- Assets/Scripts/Dropper.cs
+++ Assets/Scripts/Dropper.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Dropper : MonoBehaviour
 {
     [SerializeField] float timeToWait = 5f;
-    MeshRenderer renderer;
-    Rigidbody rigidbody;
+    new MeshRenderer renderer;
+    new Rigidbody rigidbody;
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/Scorer.cs
+++ Assets/Scripts/Scorer.cs
@@ -7,7 +7,12 @@ public class Scorer : MonoBehaviour
     int hits = 0;
     void OnCollisionEnter(Collision collision)
     {
-        hits ++ ;
-        Debug.Log("Current Score is :" + hits);
+       
+
+        if (collision.gameObject.tag != "Hit")
+        {
+            hits++;
+            Debug.Log("Current Score is :" + hits);
+        }
     }
 }
